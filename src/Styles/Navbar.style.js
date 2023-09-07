610f40c41aaa7aea8a7acd545f12daf1289ea5a4
davidzaratel <davidzaratelopez@Davids-MacBook-Pro.local>
2023-09-07 16:03:11 +0200

    Adding responsive navbar

--- src/Styles/Navbar.style.js
+++ src/Styles/Navbar.style.js
@@ -12,18 +12,27 @@ export const NavbarContainer = styled.nav `
 `;
 
 export const LeftContainer = styled.div `
-    flex: 60%;
+    flex: 70%;
     display: flex;
     align-items: center;
-    padding-left: 5%;
+    padding-left: 3%;
+    @media (max-width: 700px) {
+        flex: 10%;
+        height: 80px;
+    }
 `;
 
 export const RightContainer = styled.div `
-    flex: 40%;
+    flex: 30%;
     display: flex;
     align-items: center;
     text-align: right;
-    padding-left: 70px;
+    padding-left: 20px;
+    @media (max-width: 700px) {
+        padding-top: 20px;
+        padding-left: 0px;
+        flex: 90%;
+    }
 `;
 
 export const NavbarInnerContainer = styled.div `
@@ -41,33 +50,57 @@ export const NavBarLink = styled(Link) `
     font-size: x-large;
     font-family: Arial, Helvetica, sans-serif;
     text-decoration: none;
-    margin: 10px;
+    margin: 7%;
 
-    @media (max-width: 700px){
+    @media (max-width: 700px) {
         display: none;
+    }
+    @media (max-width: 900px) {
+        font-size: large;
     }
 `;
 
 export const NavBarLinkExtended = styled(Link) `
-    color: white;
+    color: #0D1B2A;
     font-size: x-large;
     font-family: Arial, Helvetica, sans-serif;
     text-decoration: none;
     margin: 10px;
+    @media (max-width: 450px) {
+        font-size: large;
+    }
 `;
 export const OpenLinkButton = styled.button`
     width: 70px;
     height: 50px;
     background: none;
     border: none;
-    color: white;
+    color: #0D1B2A;
     font-size: 45px;
     cursor: pointer;
 
-    @media (min-width: 700px){
+    @media (min-width: 701px) {
         display: none;
     }
 `
+
+export const NavBarHomeLink= styled(Link) `
+    color: #0D1B2A;
+    font-size: x-large;
+    font-family: Arial, Helvetica, sans-serif;
+    text-decoration: none;
+    margin: auto;
+    @media (max-width: 900px) {
+        font-size: large;
+    }
+    @media (max-width: 700px) {
+        font-size: x-large;
+        margin-right: 40%;
+    }
+    @media (max-width: 450px) {
+        font-size: large;
+    }
+`;
 
 export const NavbarExtendedContainer = styled.div `
     display: flex;

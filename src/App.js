b42ad9e082b200c42984b172fc9eedbd5bdada4b
davidzaratel <a01329785@itesm.mx>
2023-12-07 16:56:01 +0100

    changing router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router} from "react-router-dom"
+import {HashRouter as Router} from "react-router-dom"
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import AnimatedRoutes from './Components/AnimatedRoutes';
